f5c585e5af06194f48186280b98f2034db06ad61
Ananth Iyer <ananth.iyer@lifeomic.com>
2019-10-10 11:13:08 -0400

    added fhir-search

--- phc/__init__.py
+++ phc/__init__.py
@@ -1,3 +1,4 @@
 from phc.web.session import Session
 from phc.web.accounts import Accounts
 from phc.web.projects import Projects
+from phc.web.fhir_search import FhirSearch
---
+++ phc/web/fhir_search.py
@@ -0,0 +1,30 @@
+"""A Python Module for FHIR Search"""
+
+from phc.web.base_client import BaseClient
+
+
+class FhirSearch(BaseClient):
+    """Provides methods to run search using SQL or Elasticsearch queries"""
+
+    def execute_sql(self, project, statement):
+        """Executes an SQL query against fhir-searh-service
+        Returns:
+            [List] -- Dictionary with query response
+        """
+        return self.api_call(
+            api_path="fhir-search/projects/{}".format(project),
+            http_verb="POST",
+            data=statement,
+            headers={"Content-Type": "text/plain"},
+        ).data
+
+    def execute_es(self, project, query):
+        """Executes an elasticsearch query against fhir-searh-service
+        Returns:
+            [Dict] -- Dictionary with query response
+        """
+        return self.api_call(
+            api_path="fhir-search/projects/{}".format(project),
+            http_verb="POST",
+            json=query,
+        ).data
