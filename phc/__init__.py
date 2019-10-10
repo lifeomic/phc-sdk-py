82408b1d17aa06c797e1eb324d884e64e7e2aebd
Mark Schroering <mark.schroering@lifeomic.com>
2019-10-10 12:31:51 -0400

    0.2.1

--- phc/__init__.py
+++ phc/__init__.py
@@ -2,3 +2,4 @@ from phc.web.session import Session
 from phc.web.accounts import Accounts
 from phc.web.projects import Projects
 from phc.web.fhir import Fhir
+from phc.web.analytics import Analytics
