7f93f4e2febdce0cde23aee095f4eaa7fed33fd1
Christian Di Lorenzo <christian.dilorenzo@lifeomic.com>
2020-07-13 09:09:05 -0400

    Clean up filename line wrapping

--- phc/util/api_cache.py
+++ phc/util/api_cache.py
@@ -18,15 +18,25 @@ class APICache:
     @staticmethod
     def filename_for_fhir_dsl(query: dict):
         "Descriptive filename with hash of query for easy retrieval"
+        column_description = (
+            f"{len(query.get('columns', []))}col"
+            if isinstance(query.get("columns"), list)
+            else ""
+        )
+
+        where_description = "where" if query.get("where") else ""
+
+        unique_hash = hashlib.sha256(
+            json.dumps(query).encode("utf-8")
+        ).hexdigest()[0:8]
+
         components = [
             "fhir",
             "dsl",
             *[d.get("table", "") for d in query.get("from", [])],
-            f"{len(query.get('columns', []))}col"
-            if isinstance(query.get("columns"), list)
-            else "",
-            "where" if query.get("where") else "",
-            hashlib.sha256(json.dumps(query).encode("utf-8")).hexdigest()[0:8],
+            column_description,
+            where_description,
+            unique_hash,
         ]
 
         return "_".join([c for c in components if len(c) > 0]) + ".csv"
