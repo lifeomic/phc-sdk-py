aa1b577db532b33aac2a99b0d5952ea6ea2aff64
Christian Di Lorenzo <christian.dilorenzo@lifeomic.com>
2020-08-12 19:10:07 +0000

    Split datetime columns into local, tz and show progress

--- phc/easy/frame.py
+++ phc/easy/frame.py
@@ -1,8 +1,13 @@
+from toolz import curry
 from typing import Callable, List, Tuple
 
+import re
 import pandas as pd
 
 from phc.easy.codeable import Codeable
+from phc.easy.util import tqdm, update_progress
+
+TZ_REGEX = re.compile(r"-\d{2}:?\d{2}Z?$")
 
 CODE_COLUMNS = [
     "meta",
@@ -34,6 +39,21 @@ def column_to_frame(frame: pd.DataFrame, column_name: str, expand_func):
 
 
 class Frame:
+    @staticmethod
+    @curry
+    def _find_index_of_similar(columns: List[str], column_name: str):
+        "Find sort order by original frame column names"
+        MAX_INDEX = len(columns)
+
+        return next(
+            filter(
+                lambda pair: pair[1] in column_name,
+                # Start from reverse end since later columns might be longer
+                reversed(list(enumerate(columns))),
+            ),
+            (MAX_INDEX, None),
+        )[0]
+
     @staticmethod
     def codeable_like_column_expander(column_name: str):
         """Codeable expansion with prefix for passing to Frame.expand#custom_columns"""
@@ -75,37 +95,92 @@ class Frame:
         all_code_columns = [*CODE_COLUMNS, *code_columns]
         all_date_columns = [*DATE_COLUMNS, *date_columns]
 
-        codeable_col_names = [
-            col_name
-            for col_name in all_code_columns
-            if col_name in frame.columns
-        ]
-
-        code_frames = [
-            Codeable.expand_column(frame[col_name]).add_prefix(f"{col_name}.")
-            for col_name in codeable_col_names
+        codeable_column_names = [
+            key for key in all_code_columns if key in frame.columns
         ]
 
         custom_names = [
             key for key, _func in custom_columns if key in frame.columns
         ]
 
+        progress = (
+            tqdm(
+                total=(
+                    len(codeable_column_names)
+                    + len(all_date_columns)
+                    + len(custom_names)
+                ),
+                # If doing many batches, we don't want to pollute with boatloads
+                # of progress indicators around.
+                leave=False,
+            )
+            if tqdm
+            else None
+        )
+
+        code_frames = [
+            (
+                update_progress(progress, 1, col_name)
+                and Codeable.expand_column(frame[col_name]).add_prefix(
+                    f"{col_name}."
+                )
+            )
+            for col_name in codeable_column_names
+        ]
+
         columns = [
+            frame.drop([*codeable_column_names, *custom_names], axis=1),
             *[
-                column_to_frame(frame, key, func)
+                (
+                    update_progress(progress, 1, key)
+                    and column_to_frame(frame, key, func)
+                )
                 for key, func in custom_columns
             ],
-            frame.drop([*codeable_col_names, *custom_names], axis=1),
             *code_frames,
         ]
 
         combined = pd.concat(columns, axis=1)
 
-        # Mutate data frame to parse date columns
-        for column_key in all_date_columns:
-            if column_key in combined.columns:
-                combined[column_key] = pd.to_datetime(
-                    combined[column_key], utc=True
-                )
+        date_column_names = list(
+            filter(lambda k: k in combined.columns, all_date_columns)
+        )
 
-        return combined
+        # Jump past date_columns not in expanded column
+        update_progress(
+            progress,
+            len(all_date_columns) - len(date_column_names),
+            "Parsing dates...",
+        )
+
+        # Mutate data frame to parse date columns
+        for column_key in date_column_names:
+            update_progress(progress, 1, column_key)
+            local_key = f"{column_key}.local"
+            tz_key = f"{column_key}.tz"
+
+            utc = pd.to_datetime(combined[column_key], utc=True)
+
+            # Cleverness: Use regex to remove TZ and parse as utc=True to
+            # produce local datetime. The column name will have ".local" as
+            # suffix so it'll be clear what's happening.
+            localized = pd.to_datetime(
+                combined[column_key].str.replace(TZ_REGEX, ""), utc=True
+            )
+
+            combined[tz_key] = (localized - utc).dt.total_seconds() / 3600
+            combined[local_key] = localized
+
+        update_progress(progress, 0, "Finishing expansion...")
+
+        if progress:
+            progress.close()
+
+        # Sort columns by original order (where possible)
+        return combined.reindex(
+            sorted(
+                [c for c in combined.columns if c not in date_column_names],
+                key=Frame._find_index_of_similar(frame.columns),
+            ),
+            axis="columns",
+        )
