24526f286c86dc3ebd5fd940184c269553cead9f
Christian Di Lorenzo <christian.dilorenzo@lifeomic.com>
2020-08-26 17:40:28 -0400

    Add AuditEvent, CarePlan, Consent, DiagnosticReport, and MedicationStatement

---
+++ phc/easy/medication_statement.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+from phc.easy.frame import Frame
+from phc.easy.patient_item import PatientItem
+
+
+class MedicationStatement(PatientItem):
+    @staticmethod
+    def table_name():
+        return "medication_statement"
+
+    @staticmethod
+    def code_keys():
+        return [
+            "medicationCodeableConcept.coding",
+        ]
+
+    @staticmethod
+    def transform_results(df: pd.DataFrame, **expand_args):
+        return Frame.expand(
+            df,
+            date_columns=[
+                *expand_args.get("date_columns", []),
+                "effectiveDateTime",
+            ],
+            code_columns=[
+                *expand_args.get("code_columns", []),
+                "medicationCodeableConcept",
+            ],
+            custom_columns=[
+                *expand_args.get("custom_columns", []),
+                Frame.codeable_like_column_expander("subject"),
+            ],
+        )
