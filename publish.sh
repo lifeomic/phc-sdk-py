7a2975bab9257bb77e234d3c5b48b66aab43460f
Connor Luebbehusen <connor.luebbehusen@lifeomic.com>
2023-08-25 13:16:59 -0400

    PHC-5272 - fix publishing

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ does_pypi_version_exist () {
     pip3 index versions "$pkg_name" | [[ $(cat) == *$version* ]]
 }
 
-pkg_version=$(cat phc/version.py | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
+pkg_version=$(poetry version | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
 if does_pypi_version_exist phc "$pkg_version"; then
     echo "Package version $pkg_version has already been published to PyPi; exiting early"
     exit 0
@@ -16,4 +16,4 @@ poetry build -f sdist
 poetry build -f wheel
 
 echo "Publishing package version $pkg_version to PyPi"
-poetry run python -m twine upload dist/*
+# poetry run python -m twine upload dist/*
