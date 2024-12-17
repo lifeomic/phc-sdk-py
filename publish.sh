ecf327b1ccd6012e042ad8ec85da4aa2cd122606
Evan Peterson <evan.peterson@fountainlife.com>
2024-12-17 15:01:50 -0700

    Fix release workflow

--- publish.sh
+++ publish.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-does_pypi_version_exist () {
-    pkg_name=$1; version=$2
+does_pypi_version_exist() {
+    pkg_name=$1
+    version=$2
     pip3 index versions "$pkg_name" | [[ $(cat) == *$version* ]]
 }
 
@@ -12,8 +13,7 @@ if does_pypi_version_exist phc "$pkg_version"; then
 fi
 
 echo "Building the package"
-poetry build -f sdist
-poetry build -f wheel
+poetry build
 
 echo "Publishing package version $pkg_version to PyPi"
-poetry run python -m twine upload dist/*
+poetry publish
